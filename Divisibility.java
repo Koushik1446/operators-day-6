f2d39e85a944c77c2a031f708eaef0e6f1743b2d
Koushik1446 <saikoushiknookala144@gmail.com>
2025-10-16 23:36:32 +0530

    assignment

---
+++ Divisibility.java
@@ -0,0 +1,17 @@
+package com.codegnanoperatorexamples;
+
+import java.util.Scanner;
+
+public class Divisibility {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter the number:");
+		int i = scanner.nextInt();
+		String result = (i%5>0)?"it is not divisible by 5":"it is divisible by 5";
+		System.out.println(result);
+		scanner.close();
+	}
+
+}
